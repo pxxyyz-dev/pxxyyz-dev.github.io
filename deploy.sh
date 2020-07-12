ee480f708dfd6eb80af4cfffbaca5c96a75a8a66
pxxyyz <pxxyyz@gmail.com>
2020-07-12 13:49:04 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,8 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
-git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'Update'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:pxxyyz-dev/pxxyyz-dev.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
+git push -f git@github.com:pxxyyz-dev/pxxyyz-dev.github.io.git source
